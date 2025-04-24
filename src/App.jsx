5de7fd2e9db84265637bb01b9a0ff6718a12dc63
Daniel07-10 <daniel.banol58@gmail.com>
2025-04-24 12:35:45 -0500

    creacion e iniciacion de proyecto

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import {Header} from './layouts/Header/Header'
+
+
+
+
+export const  App = () => {
+
+  return (
+    <>
+      <Header/>
+      
+
+    </>
+  )
+}
+
+
---
+++ src/layouts/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Header = () => {
+  return (
+    <header className="bg-[#1a2238] w-full text-white py-6 text-center">
+      <h1 className="text-4xl font-bold text-yellow-400">
+        Consumo de Api
+      </h1>
+      
+    </header>
+  );
+};
