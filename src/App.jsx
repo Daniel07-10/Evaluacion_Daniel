d286e5cff95177bd2c73ce4f687307bf1ff8b0c0
Daniel07-10 <daniel.banol58@gmail.com>
2025-04-24 14:21:31 -0500

    ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import {Header} from './layouts/Header/Header'
 import {Footer} from './layouts/Footer/Footer'
 import { MainContainer } from './layouts/MainContainer/MainContainer'
-import { InputFiltro } from './components/InputFiltro/InputFiltro'
+
 
 
 export const  App = () => {
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,8 +5,8 @@ export const Card = ({title,price,image,description}) => {
         <div className="bg-white rounded-2xl shadow-md overflow-hidden hover:shadow-xl transition-shadow duration-300 flex flex-col">
           <img src={image} alt={title} className="h-48 object-contain p-4" />
           <div className="p-4 flex-1 flex flex-col justify-between">
-            <h2 className="text-lg font-bold text-gray-800 mb-2">{title}</h2>
-            <p className="text-sm text-gray-600 mb-3 line-clamp-2">{description}</p>
+            <h2 className="text-lg font-bold text-gray-800 ">{title}</h2>
+            <p className="text-sm text-gray-600  text-justify">{description}</p>
             <span className="text-indigo-600 font-semibold text-lg">${price}</span>
           </div>
         </div>
